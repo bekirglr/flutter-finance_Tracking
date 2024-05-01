e2630ac3c2082dc36a116ae899b508ff889c9148
bekirglr <bbekirglr@gmail.com>
2024-05-01 22:45:30 +0300

    changed ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 import 'screens/home_screen.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Finance App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        iconTheme: IconThemeData(
-          color: Colors.green.shade700,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          brightness: Brightness.light,
+          seedColor: Colors.green.shade800,
         ),
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import '../screens/rapor.dart';
-import '../widgets/draggablesheet_panel.dart';
+import 'package:simple_speed_dial/simple_speed_dial.dart';
+import '../services/firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,118 +13,129 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final TextEditingController _salaryController = TextEditingController();
+  //Firestore
+  final FirestoreSevice firestoreSevice = FirestoreSevice();
 
-  @override
-  void dispose() {
-    _salaryController.dispose();
-    super.dispose();
+  //text controller
+  final TextEditingController textController = TextEditingController();
+
+  //open a dialog box to add a note
+  void openNoteBox({String? docID}) {
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+              content: TextField(
+                controller: textController,
+              ),
+              actions: [
+                ElevatedButton(
+                  onPressed: () => {
+                    //add a new note
+                    if (docID == null)
+                      {firestoreSevice.addNote(textController.text)}
+                    //update an existing note
+                    else
+                      {firestoreSevice.updateNote(docID, textController.text)},
+
+                    //clear the text controller
+                    textController.clear(),
+
+                    //close the box
+                    Navigator.pop(context),
+                  },
+                  child: Text("Ekle"),
+                )
+              ],
+            ));
   }
 
   @override
   Widget build(BuildContext context) {
+    String _text = "";
     return Scaffold(
-      backgroundColor: const Color.fromARGB(238, 238, 238, 238),
-      appBar: _buildAppBar(),
-      drawer: _buildDrawer(),
-      body: DraggableSheetPanel(),
-    );
-  }
-
-  AppBar _buildAppBar() {
-    return AppBar(
-      leading: IconButton(
-        icon: const Icon(Icons.settings),
-        onPressed: () {
-          Scaffold.of(context).openDrawer();
-        },
-        tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-      ),
-      centerTitle: true,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(25),
-      ),
-      actions: [
-        _buildSalaryInput(),
-        IconButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ToReportScreen()),
-            );
-          },
-          icon: const Icon(
-            Icons.receipt_long,
-            size: 25,
-          ),
+        appBar: AppBar(
+          title: Text(_text),
+          centerTitle: true,
         ),
-      ],
-      flexibleSpace: Container(
-        decoration: BoxDecoration(
-          boxShadow: const [
-            BoxShadow(
-              blurRadius: 5,
-              spreadRadius: 1,
-              offset: Offset(0, 0),
-            )
-          ],
-          borderRadius: BorderRadius.circular(25),
-          color: Colors.white,
-        ),
-      ),
-    );
-  }
-
-  Widget _buildSalaryInput() {
-    return SizedBox(
-      width: 100,
-      child: TextField(
-        controller: _salaryController,
-        keyboardType: TextInputType.number,
-        decoration: const InputDecoration(
-          suffix: Icon(
-            FontAwesomeIcons.turkishLiraSign,
-            size: 20,
-          ),
-          hintText: "Miktar Giriniz",
-          hintStyle: TextStyle(
-            color: Color.fromARGB(166, 158, 158, 158),
-            fontSize: 20,
-          ),
-          border: InputBorder.none,
-        ),
-        style: const TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.w900,
-          color: Colors.black,
-        ),
-        textAlign: TextAlign.right,
-        onChanged: (value) {},
-      ),
-    );
-  }
-
-  Drawer _buildDrawer() {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
+        floatingActionButton: SpeedDial(
+          openBackgroundColor: Colors.green.shade200,
+          labelsBackgroundColor: Colors.white,
+          speedDialChildren: [
+            SpeedDialChild(
+              child: const Icon(FontAwesomeIcons.arrowTrendUp),
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.green.shade500,
+              label: 'Tutar Ekle',
+              onPressed: () {},
             ),
-            child: Text('Drawer Header'),
-          ),
-          ListTile(
-            title: const Text('Item 1'),
-            onTap: () {},
-          ),
-          ListTile(
-            title: const Text('Item 2'),
-            onTap: () {},
-          ),
-        ],
-      ),
-    );
+            SpeedDialChild(
+                child: const Icon(FontAwesomeIcons.arrowTrendDown),
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.red.shade500,
+                label: 'Tutar Çıkar',
+                onPressed: () {}),
+            SpeedDialChild(
+              child: const Icon(FontAwesomeIcons.handHoldingDollar),
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.blue.shade500,
+              label: 'Bakiye Oluştur',
+              onPressed: () {},
+            ),
+          ],
+          child: Icon(Icons.menu),
+        ),
+        body: StreamBuilder<QuerySnapshot>(
+            stream: firestoreSevice.getNoteStream(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                List notesList = snapshot.data!.docs;
+                return Column(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.4,
+                      child: TextField(
+                        controller: textController,
+                        textAlign: TextAlign.left,
+                        keyboardType:
+                            TextInputType.numberWithOptions(decimal: true),
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                          itemCount: notesList.length,
+                          itemBuilder: (context, index) {
+                            DocumentSnapshot document = notesList[index];
+                            String docID = document.id;
+                            Map<String, dynamic> data =
+                                document.data() as Map<String, dynamic>;
+                            String noteText = data['note'];
+                            return ListTile(
+                                title: Text(noteText),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                        onPressed: () =>
+                                            openNoteBox(docID: docID),
+                                        icon: Icon(Icons.settings)),
+                                    IconButton(
+                                        onPressed: () =>
+                                            firestoreSevice.deleteNote(docID),
+                                        icon: Icon(Icons.delete))
+                                  ],
+                                ));
+                          }),
+                    ),
+                  ],
+                );
+              } else {
+                return Center(
+                  child: Text("No data..."),
+                );
+              }
+            }));
   }
 }
